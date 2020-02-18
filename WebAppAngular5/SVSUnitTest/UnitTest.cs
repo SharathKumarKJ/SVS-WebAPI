9fa6b63cc1b320bfbf8f00bae0460e43a4ae06f6
sharath.jayaramegowda@jda.com <sharath.jayaramegowda@jda.com>
2020-02-18 18:38:55 +0530

    Unit Test updated

--- WebAppAngular5/SVSUnitTest/UnitTest.cs
+++ WebAppAngular5/SVSUnitTest/UnitTest.cs
@@ -12,42 +12,6 @@ namespace SVSUnitTest
         public void TestMethod()
         {
             var rand = new Random();
-            var id = rand.Next(1,1000);
-
-            var user = new AccountModel
-            {
-                UserName = id + "JDASoftware",
-                Password = "jda@123",
-                Email = id + "jda@gmail.com",
-                FirstName = id + "JDA",
-                LastName = "Software",
-                Role = "SecurityAdmin"
-            };
-            var account = new AccountController();
-
-            Assert.AreEqual(user, user);
-        }
-        public void TestMethod2()
-        {
-            var rand = new Random();
-            var id = rand.Next(1, 1000);
-
-            var user = new AccountModel
-            {
-                UserName = id + "JDASoftware",
-                Password = "jda@123",
-                Email = id + "jda@gmail.com",
-                FirstName = id + "JDA",
-                LastName = "Software",
-                Role = "SecurityAdmin"
-            };
-            var account = new AccountController();
-
-            Assert.AreEqual(user, user);
-        }
-        public void TestMethod3()
-        {
-            var rand = new Random();
             var id = rand.Next(1, 1000);
 
             var user = new AccountModel
